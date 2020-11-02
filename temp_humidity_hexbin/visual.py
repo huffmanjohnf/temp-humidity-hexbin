d11195b6c024ffe600e12eeec1f88b86959513da
huffmanjohnf <huffmanjohnf@gmail.com>
2020-11-02 16:53:15 -0500

    Add auto plot defaults (#9)
    * enable adjustable set points
    
    * automatically generate plot defaults

--- temp_humidity_hexbin/visual.py
+++ temp_humidity_hexbin/visual.py
@@ -1,8 +1,46 @@
+from typing import Tuple
+
 import pandas as pd
 import seaborn as sns
 
 
-def hexbin_plt(df: pd.DataFrame, sp_cool: float = 72.0, sp_heat: float = 68.0, sp_humi: float = 55.0):
+def get_range(S: pd.Series):
+    return (S.quantile(0.01), S.quantile(0.99))
+
+
+def find_edge(considerations, how: str = "max", buffer: int = 1):
+    assert how in ["min", "max"]
+    if how == "min":
+        return min(considerations) - buffer
+    else:
+        return max(considerations) + buffer
+
+
+def gridsize(x_01: float, x_99: float, y_01: float, y_99: float, scale: int = 2):
+    xgrid = x_99 - x_01
+    ygrid = y_99 - y_01
+    correction_factor = min([xgrid, ygrid])
+    xgrid = max([int(xgrid * (correction_factor / xgrid) * scale), 1])
+    ygrid = max([int(ygrid * (correction_factor / ygrid) * scale), 1])
+    return (ygrid, xgrid)
+
+
+def hexbin_plt(
+    df: pd.DataFrame,
+    sp_cool: float = 72.0,
+    sp_heat: float = 68.0,
+    sp_humi: float = 55.0,
+    xlim: Tuple[int] = None,
+    ylim: Tuple[int] = None,
+):
+    x_range = get_range(df["Temperature"])
+    y_range = get_range(df["Humidity"])
+
+    if not xlim:
+        xlim = (find_edge([sp_heat, x_range[0]], "min"), find_edge([sp_cool, x_range[1]], "max"))
+    if not ylim:
+        ylim = (y_range[0], find_edge([sp_humi, y_range[1]], "max"))
+
     hexbin = sns.jointplot(
         x="Temperature",
         y="Humidity",
@@ -12,8 +50,9 @@ def hexbin_plt(df: pd.DataFrame, sp_cool: float = 72.0, sp_heat: float = 68.0, s
         ratio=15,
         space=0,
         edgecolor="w",
-        xlim=(64, 76),
-        ylim=(45, 65),
+        xlim=xlim,
+        ylim=ylim,
+        joint_kws=dict(gridsize=gridsize(*x_range, *y_range)),
         color="k",
     )
 
