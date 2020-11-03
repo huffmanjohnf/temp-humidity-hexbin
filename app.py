5741c8ac8c59666be374db62fce16d8d575c9bfc
John Huffman <huffmanjohnf@gmail.com>
2020-11-03 11:51:26 -0500

    move app inputs to sidebar

--- app.py
+++ app.py
@@ -3,11 +3,19 @@ import streamlit as st
 from temp_humidity_hexbin.data import join, load_data, rh2dp
 from temp_humidity_hexbin.visual import hexbin_plt
 
-st.title("Temperature vs. Humidity Hexbin")
+st.set_page_config(
+    page_title="Hexbin App",
+    layout="centered",
+    initial_sidebar_state="expanded",
+)
 
-temperature_file = st.file_uploader("Upload temperature data file", type=["csv"])
-humidity_file = st.file_uploader("Upload himidity data file", type=["csv"])
-humidity_type = st.selectbox("Select the type of humidity data:", ["Select", "Dew Point", "Relative Humidity"])
+st.title("Temperature vs. Humidity Hexbin Plot")
+
+st.sidebar.subheader("Upload data files:")
+temperature_file = st.sidebar.file_uploader("Upload temperature data file", type=["csv"])
+humidity_file = st.sidebar.file_uploader("Upload himidity data file", type=["csv"])
+st.sidebar.subheader("Specify uploaded humidity type:")
+humidity_type = st.sidebar.selectbox("", ["Select", "Dew Point", "Relative Humidity"])
 
 if not humidity_file or not humidity_file or humidity_type == "Select":
     st.stop()
@@ -15,7 +23,7 @@ if not humidity_file or not humidity_file or humidity_type == "Select":
 df_t = load_data(temperature_file).rename(columns={"RawValue": "Temperature"})
 df_h = load_data(humidity_file).rename(columns={"RawValue": "Humidity"})
 
-col1, col2 = st.beta_columns(2)
+col1, col2 = st.beta_columns([2, 3])
 
 with col1:
     plt_params = {}
@@ -24,15 +32,15 @@ with col1:
     plt_params["sp_cool"] = st.slider("Cooling Set Point (\N{DEGREE SIGN}F)", 68.0, 76.0, 72.0, 0.5)
     plt_params["sp_humi"] = st.slider("Humidity Set Point (Dew Point, \N{DEGREE SIGN}F)", 45.0, 65.0, 55.0, 0.5)
 
-    with st.beta_expander("Overrides"):
+    with st.beta_expander("Override Plot Window"):
         x_override = st.checkbox("Override X-axis")
-        x1 = st.number_input("X-axis Range", min_value=20, max_value=100, value=64)
+        x1 = st.number_input("X-axis range", min_value=20, max_value=100, value=64)
         x2 = st.number_input("", min_value=20, max_value=100, value=76)
         if x_override:
             plt_params["xlim"] = (min([x1, x2]), max([x1, x2]))
 
         y_override = st.checkbox("Override Y-axis")
-        y1 = st.number_input("Y-axis Range", min_value=20, max_value=100, value=45)
+        y1 = st.number_input("Y-axis range", min_value=20, max_value=100, value=45)
         y2 = st.number_input("", min_value=20, max_value=100, value=65)
         if y_override:
             plt_params["ylim"] = (min([y1, y2]), max([y1, y2]))
